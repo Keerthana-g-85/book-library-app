476a65e7e920fb25afe64815d9caf4150d9209c2
Keerthana G <keerthana@CELSYSBLR000337.local>
2026-05-04 13:32:53 +0530

    adding eslint rules

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -20,7 +20,15 @@ export default defineConfig([
     rules: {
       "no-unused-vars": "warn",
       "no-console": "off",
-      "eqeqeq": "error"
+      "eqeqeq": "error",
+      "no-var": "error",                   // Force let/const
+      "prefer-const": "warn",              // Suggest const where possible
+      "no-duplicate-imports": "error",      // Clean up imports
+      "curly": ["error", "all"],           // Prevent logic bugs in loops/ifs
+      "no-multiple-empty-lines": ["warn", { "max": 1 }], // Keep code compact
+      "semi": ["error", "always"],         // Enforce semicolons for consistency
+      "quotes": ["error", "single"],       // Enforce single quotes
+      "camelcase": ["warn", { "properties": "always" }] // Enforce camelCase naming
     }
   }
 ]);
